0a5ceecf451cde1ac79a54667e92a30fefca42e4
helen88 <z8hanghuan@126.com>
2015-09-05 23:12:56 +0800

    * file name need to be escape. -- to do *

--- mini_spider.py
+++ mini_spider.py
@@ -20,31 +20,59 @@ class WebSpider(object):
         self.url = url
         self.protocol = ''
 
-    def get_html(self):
+    def get_page_source(self):
         if self.url is None or self.url == " ":
             return
 
-        try:
-            html = self.handle_gzip_file()
-            print "gzipped file~"
-        except:
-            html = self.handle_utf8_page()
-            print "not gzipped file~"
+        # if self.url.split('.')[1] == 'baidu': #不够智能
+        #     html = self.handle_not_gzipped_file()
+        #     print "not gzipped file~"
+        # else:
+        #     html = self.handle_gzipped_file()  #if gzipped file
+        #     print "gzipped file~"
+        #
+        # return html
+        #
 
-        # print html 
-        return html
-
-    def handle_utf8_page(self):
-        #url = 'http://www.baidu.com'
         #有的网站禁止爬虫，不能抓取或者抓取一定数量后封ip, 解决：伪装成浏览器进行抓取，加入headers
         headers = {'Use-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
         request = urllib2.Request(self.url, headers=headers)
-        response = urllib2.urlopen(request)#打开url并返回类文件对象
-        html = response.read()
-        print "utf-8 page http_status:", response.getcode()
-        print "实际的url:", response.geturl() #返回所返回的数据的实际url,但会考虑发生的重定向问题
+        response = urllib2.urlopen(request)
+        print "http_status:", response.getcode()
+        print "实际url:", response.geturl()
         self.protocol = response.geturl().split(':')[0]
-        return html
+
+        try:
+            data = response.read()
+            data = StringIO.StringIO(data)
+            #其实，现在很多网页为了提高浏览器端用户的访问速度，和搜索引擎爬虫抓取的速度，都在使用gzip压缩。
+            gzip_file = gzip.GzipFile(fileobj=data)
+            html = gzip_file.read()
+            #fromEncoding 原网页的编码，当然也可以写一个自动获取网页编码传进来更好
+            page = bs4.BeautifulSoup(html, from_encoding='GB18030')
+            print type(page) #<class 'bs4.BeautifulSoup'>
+            #折打tag,里面是排好的网页源代码
+            # print page
+        except Exception, e:
+            print e
+            # request = urllib2.Request(url, headers=headers)
+            response = urllib2.urlopen(request)
+            page = response.read()
+            # print page
+
+        return ["OK", page]
+
+    # def handle_not_gzipped_file(self):
+    #     #not gzip file: http://www.baidu.com
+    #     #有的网站禁止爬虫，不能抓取或者抓取一定数量后封ip, 解决：伪装成浏览器进行抓取，加入headers
+    #     headers = {'Use-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    #     request = urllib2.Request(self.url, headers=headers)
+    #     response = urllib2.urlopen(request)#打开url并返回类文件对象
+    #     html = response.read()
+    #     print "utf-8 page http_status:", response.getcode()
+    #     print "实际的url:", response.geturl() #返回所返回的数据的实际url,但会考虑发生的重定向问题
+    #     self.protocol = response.geturl().split(':')[0]
+    #     return html
 
     def is_https(self):
         if self.protocol == 'https':
@@ -52,26 +80,26 @@ class WebSpider(object):
         else:
             return False
 
-    def handle_gzip_file(self):
-        headers = {'Use-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-        request = urllib2.Request(url, headers=headers)
-        response = urllib2.urlopen(request)
-        print "gzip_file http_status: ", response.getcode()
-        print "实际url:", response.geturl()
-        self.protocol = response.geturl().split(':')[0]
-
-        data = response.read()
-        data = StringIO.StringIO(data)
-
-        #现在很多网页为了提高浏览器端用户的访问速度，和搜索引擎爬虫抓取的速度，都在使用gzip压缩。
-        gzip_file = gzip.GzipFile(fileobj=data)
-        html = gzip_file.read()
-        #fromEncoding 原网页的编码，当然也可以写一个自动获取网页编码传进来更好
-        html = bs4.BeautifulSoup(html, from_encoding='GB18030') #gbk is also OK
-        # print "内容是:", html
-        # print "str(html)内容是:", str(html)
-        print "gzipped page type is:", type(html)#<class 'bs4.BeautifulSoup'>
-        return html
+    # def handle_gzipped_file(self):
+    #     headers = {'Use-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    #     request = urllib2.Request(url, headers=headers)
+    #     response = urllib2.urlopen(request)
+    #     print "gzip_file http_status: ", response.getcode()
+    #     print "实际url:", response.geturl()
+    #     self.protocol = response.geturl().split(':')[0]
+    #
+    #     data = response.read()
+    #     data = StringIO.StringIO(data)
+    #
+    #     #现在很多网页为了提高浏览器端用户的访问速度，和搜索引擎爬虫抓取的速度，都在使用gzip压缩。
+    #     gzip_file = gzip.GzipFile(fileobj=data)
+    #     html = gzip_file.read()
+    #     #fromEncoding 原网页的编码，当然也可以写一个自动获取网页编码传进来更好
+    #     html = bs4.BeautifulSoup(html, from_encoding='GB18030') #gbk is also OK
+    #     # print "内容是:", html
+    #     # print "str(html)内容是:", str(html)
+    #     print "gzipped page type is:", type(html)#<class 'bs4.BeautifulSoup'>
+    #     return html
 
     #根据正则获取图片的url列表
     def get_img_list(self, html):
@@ -99,11 +127,12 @@ class WebSpider(object):
             else:
                 img_url = img_url[0]
             print "图片链接: ", img_url
-            file_name = img_url.split("/")[-1]
+            # file_name = img_url.split("/")[-1]
+            file_name = img_url
             output_dir = config.get("spider", "output_directory")
             output_path = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), output_dir, p))
             #新建目录存放爬取的图片
-            child_dir = self.url.split('.')[1]
+            child_dir = self.url.strip().split('//')[1]
             output_sec_dir = output_path(child_dir) #/Users/will/Android/Python/good_coder/output/baidu
             print output_sec_dir
             if not os.path.exists(output_sec_dir):
@@ -113,6 +142,7 @@ class WebSpider(object):
             if not os.path.exists(os.path.abspath(os.path.join(output_sec_dir, file_name))):
                 #下载文件
                 urllib.urlretrieve(img_url, os.path.abspath(os.path.join(output_sec_dir, file_name)))
+                # urllib.urlretrieve(img_url, file_name)
             else:
                 print file_name + " 已经存在了~"
             CRAWL_INTERVAL = config.get("spider", "crawl_interval")
@@ -120,8 +150,62 @@ class WebSpider(object):
 
 
 class Queue(object):
+    def __init__(self, tmp_list=[]):
+        self.queue = tmp_list
+
+    #入队
+    def push(self, v):
+        self.queue.append(v)
+        print "enqueue:", v
+
+    #出队
+    def pop(self):
+        return self.queue.pop(0) #pop method returns the removed object from the list.
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def get_length(self):
+        return len(self.queue)
+
+
+class LinkQueue(object):
     def __init__(self):
-        pass
+        self.visited = Queue()
+        self.unvisited = Queue()
+
+    def get_visited_url(self):
+        return self.visited
+
+    def get_unvisited_url(self):
+        return self.unvisited
+
+    #添加到访问过的url队列中
+    def add_visited_url(self, url):
+        self.visited.push(url)
+
+    #为什么要删除访问过的url呢??
+    def remove_visited_url(self, url):
+        self.visited.remove(url)
+
+    def add_unvisited_url(self, url):
+        #确保每个url只能被访问一次
+        is_visited = (url != " " and url in self.visited and url not in self.unvisited)
+        if not is_visited:
+            # self.unvisited.insert(0, url)
+            self.unvisited.push(url)
+
+    def get_visited_url_length(self):
+        return self.visited.get_length()
+
+    def get_unvisited_url_length(self):
+        return self.unvisited.get_length()
+
+    def is_unvisited_url_empty(self):
+        if self.unvisited.is_empty():
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
@@ -151,14 +235,14 @@ if __name__ == "__main__":
 
     # log.init_log("./log/mini_spider")
     # logging.info("Start~")
-    print url_arr[1]
-    web_spider = WebSpider(url_arr[1])
-    # html = web_spider.get_html()
+    print "当前处理url: ",url_arr[0]
+    web_spider = WebSpider(url_arr[0])
+    # html = web_spider.get_page_source()
 
     #如果为https请求就再请求一次
     count = 1
     while True:
-        html = web_spider.get_html()
+        html = web_spider.get_page_source()
         print "till now, total request times:", count, "times"
 
         if not web_spider.is_https():
--- test/test_bs4.py
+++ test/test_bs4.py
@@ -1,32 +1,137 @@
 #coding:utf-8
 #this way is good.
-# http://www.sina.com.cn的网页能显示出来了.
+# http://www.sina.com.cn的网页能显示出来了. 解决了中文乱码
 
 import gzip
 import StringIO
 import urllib2
 import bs4  #抓取网页中的中文乱码
-
-# url = "http://www.sina.com.cn"
-url = "http://www.baidu.com"
-headers = {'Use-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-
-request = urllib2.Request(url, headers=headers)
-response = urllib2.urlopen(request)
-data = response.read()
-data = StringIO.StringIO(data)
-
-#其实，现在很多网页为了提高浏览器端用户的访问速度，和搜索引擎爬虫抓取的速度，都在使用gzip压缩。
-gzip_file = gzip.GzipFile(fileobj=data)
-try:
-    html = gzip_file.read()
-    #fromEncoding 原网页的编码，当然也可以写一个自动获取网页编码传进来更好
-    page = bs4.BeautifulSoup(html, from_encoding='gbk')
-except Exception, e:
-    print "有异常:", e
+import re
 
 
+class Queue(object):
+    def __init__(self, tmp_list=[]):
+        self.queue = tmp_list
 
-print type(page) #<class 'bs4.BeautifulSoup'>
-#折打tag,里面是排好的网页源代码
-print page
+    #入队
+    def push(self, v):
+        self.queue.append(v)
+        print "enqueue:", v
+
+    #出队
+    def pop(self):
+        return self.queue.pop(0) #pop method returns the removed object from the list.
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def get_length(self):
+        return len(self.queue)
+
+    def is_exist(self, url):
+        if url in self.queue:
+            return True
+        else:
+            return False
+
+
+class LinkQueue(object):
+    def __init__(self, tmp_list=[]):
+        self.visited = Queue()
+        self.unvisited = Queue(tmp_list)
+
+    def get_visited_url(self):
+        return self.visited
+
+    def get_unvisited_url(self):
+        return self.unvisited
+
+    #添加到访问过的url队列中
+    def add_visited_url(self, url):
+        self.visited.push(url)
+
+    #为什么要删除访问过的url呢??
+    def remove_visited_url(self, url):
+        self.visited.remove(url)
+
+    def add_unvisited_url(self, url):
+        #添加到未访问队列,确保每个url只能被访问一次
+        if url != " " and not self.visited.is_exist(url) and not self.unvisited.is_exist(url):
+            # self.unvisited.insert(0, url)
+            self.unvisited.push(url)
+
+    def get_visited_url_length(self):
+        return self.visited.get_length()
+
+    def get_unvisited_url_length(self):
+        return self.unvisited.get_length()
+
+    def is_unvisited_url_empty(self):
+        if self.unvisited.is_empty():
+            return True
+        else:
+            return False
+
+
+def get_page_source(url):
+    # url = "http://www.sina.com.cn"
+    # url = "http://www.baidu.com"  #not a gizpped file~
+    headers = {'Use-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    request = urllib2.Request(url, headers=headers)
+    response = urllib2.urlopen(request)
+
+    try:
+        data = response.read()
+        data = StringIO.StringIO(data)
+        #其实，现在很多网页为了提高浏览器端用户的访问速度，和搜索引擎爬虫抓取的速度，都在使用gzip压缩。
+        gzip_file = gzip.GzipFile(fileobj=data)
+        html = gzip_file.read()
+        #fromEncoding 原网页的编码，当然也可以写一个自动获取网页编码传进来更好
+        page = bs4.BeautifulSoup(html, from_encoding='gbk')
+        print type(page) #<class 'bs4.BeautifulSoup'>
+        #折打tag,里面是排好的网页源代码
+        # print page
+    except Exception, e:
+        print e
+        # request = urllib2.Request(url, headers=headers)
+        response = urllib2.urlopen(request)
+        page = response.read()
+        # print page
+
+    return ["OK", page]
+
+
+def get_hyperlinks(url):
+    data = get_page_source(url)
+    if data[0] == "OK":
+        # print type(data[1])
+        # print data[1]
+        soup = bs4.BeautifulSoup(str(data[1]))
+        urls = []
+        for link in soup.find_all('a'):
+            # print link
+            try:
+                if str(link.get('href')).find("http://") != -1:
+                    urls.append(link.get('href'))
+                    # print link.get('href')
+            except Exception, err:
+                print type(err),
+                print err #'ascii' codec can't encode characters in position 33-34: ordinal not in range(128)
+
+        print "超链接长度: ", len(urls)
+        return urls
+
+if __name__ == "__main__":
+    seeds = ["http://www.baidu.com", "http://www.sina.com.cn"]
+    # get_page_source(urls[0])
+
+    #用种子初始化unvisited队列
+    link_queue = LinkQueue(seeds)
+    for seed in seeds:
+        urls = get_hyperlinks(seed)
+        for url in urls:
+            link_queue.add_unvisited_url(url)
+
+        print "till now", link_queue.get_unvisited_url_length()
+
+
